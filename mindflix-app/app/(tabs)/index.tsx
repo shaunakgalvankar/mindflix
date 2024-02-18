e6f859e130b092c46307bfb7d7c1b19f4a2a17a6
shaunak galvankar <sgalvankar@scu.edu>
2024-02-17 17:46:56 -0800

    Remove unused code

--- mindflix-app/app/(tabs)/index.tsx
+++ mindflix-app/app/(tabs)/index.tsx
@@ -14,12 +14,10 @@ export default function TabOneScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
       <StatusContainer items={items} />
 
-      <RingsComponent fillValue={[50, 55, 60, 20]} />
+
     </View>
   );
 }
