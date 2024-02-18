ca1f4c17cf6031e2ff9bf0faea5d5163204251e1
rrajurkar <ranirajurkar6111999@gmail.com>
2024-02-17 16:39:05 -0800

    Dark Theme

--- mindflix-app/app/(tabs)/index.tsx
+++ mindflix-app/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, useColorScheme } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import RingsComponent from '../../components/rings';
@@ -10,11 +10,13 @@ const items =  { title: ['Move'], achieved: [10], goals :[100]};
 
 
 export default function TabOneScreen() {
+  const scheme = useColorScheme();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
       <StatusContainer items={items} />
 
       <RingsComponent fillValue={[50, 55, 60, 20]} />
@@ -27,14 +29,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#121212',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#ffffff',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+    backgroundColor: "rgba(255,255,255,0.1)",
   },
 });
--- mindflix-app/app/(tabs)/two.tsx
+++ mindflix-app/app/(tabs)/two.tsx
@@ -9,6 +9,9 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <StatusContainer items={{ title: ["Shaunak"], achieved: [], goals: [] }} />
+      <Text style={styles.title}>Tab Two</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
     </View>
   );
 }
@@ -18,14 +21,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#121212',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#ffffff',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+    backgroundColor: "rgba(255,255,255,0.1)",
   },
 });
