c4523be338b2825a7d020465576bcca19684274a
shaunak galvankar <sgalvankar@scu.edu>
2024-02-17 17:24:24 -0800

    Add pluscircle icon to TabTwoScreen

--- mindflix-app/app/(tabs)/two.tsx
+++ mindflix-app/app/(tabs)/two.tsx
@@ -3,11 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import StatusContainer from '../../components/StatusContainer'; // Adjust the path as needed
-
+import { AntDesign } from '@expo/vector-icons';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
+      <AntDesign name="pluscircle" size={24} color="#b6fc32"  style={styles.icon}/>
       <StatusContainer items={{ title: ["Shaunak"], achieved: [], goals: [] }} />
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
@@ -33,5 +34,10 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
     backgroundColor: "rgba(255,255,255,0.1)",
+  },
+  icon: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
   },
 });
